da32bb4af10b6ccfc9b76a4f897900dd4e70b566
Joana477 <lawerjoana477@gmail.com>
2020-09-11 02:20:27 +0000

    Add backend routes. Create flask file for backend

---
+++ templates/blog.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('/index.html')
+
+@app.route('/about')
+def about():
+    return render_template('/profile.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('/projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('/contact.html') 
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
