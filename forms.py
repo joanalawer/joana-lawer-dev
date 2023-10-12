955a2b94fa415eb4bc699ed853616a1242a17e89
joanalawer <lawerjoana477@gmail.com>
2023-10-12 09:47:39 +0000

    Delete virtual env

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
-
-class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=15)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    message = StringField('Your Message', validators=[DataRequired(), Length(min=2, max=150)])
-    submit = SubmitField('Submit')
