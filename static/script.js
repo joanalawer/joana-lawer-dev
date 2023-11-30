f1e754833a4c3ac43af529e19fa3995d79c1aded
joanalawer <lawerjoana477@gmail.com>
2023-11-30 21:39:44 +0000

    Separate service modals

--- static/script.js
+++ static/script.js
@@ -25,32 +25,25 @@ window.onclick = function(event) {
   }
 }
 
-// SERVICES MODAL SECTION
-// Get the modal
-var modal = document.getElementById("service-modal");
-
-// Get the button that opens the modal
-var icon = document.getElementById("icon");
-
-// Get the <span> element that closes the modal
+//*************************SERVICES MODAL SECTION ***************************** //
+var modal = document.getElementById("analysis-modal");
+var icon = document.getElementById("chart-icon");
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 icon.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+//*************************SERVICES MODAL ENDS ***************************** //
 
 // Toggle navbar icon
 let menuIcon = document.querySelector('#menu-icon');
